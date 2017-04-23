22a5c9e349df1a2bd0e352bf1f08e3291051dac5
pfgherardini <federico.gherardini@gmail.com>
2017-04-23 15:11:56 -0700

    completed refactoring of IMCData

--- app/stores/ImageStore.tsx
+++ app/stores/ImageStore.tsx
@@ -1,8 +1,6 @@
 import { observable, computed, action, autorun, createTransformer } from "mobx"
 import Jimp = require("jimp")
-
-import * as Papa from "papaparse"
-import { IMCData, IMCDataObject } from "../interfaces/IMCData"
+import { IMCData, IMCDataObject } from "../lib/IMCData"
 import * as _ from "underscore"
 import * as d3 from "d3-array"
 import { ChannelName, D3BrushExtent, SelectOption } from "../interfaces/UIDefinitions"
@@ -83,43 +81,6 @@ export class ImageStore {
     }
 
 
-    /*
-
-    @action parseData(parseResult:PapaParse.ParseResult) {
-        let data = parseResult.data
-
-
-
-        interface parsedData {
-            [key:string] :number
-        }
-        let d:parsedData = data[0]
-
-        let imageData = new IMCData
-
-        _.mapObject(d, (val, key) => {
-            imageData.stats[key] = [val, val]
-            imageData.data[key] = new Array(data.length)
-        })
-        
-        for(let i = 0; i < data.length; ++i) {
-            let temp:parsedData = data[i]
-            _.mapObject(temp, (val, key) => {
-                if(val < imageData.stats[key][0])
-                    imageData.stats[key][0] = val
-                if(val > imageData.stats[key][1])
-                    imageData.stats[key][1] = val
-                imageData.data[key][i] = val
-            })
-
-        }
-        
-        _.mapObject(imageData.data, (val, key) => {
-            imageData.sortedData[key] = val.slice().sort((a, b) => a - b)
-        })
-        this.imageData = imageData
-    }
-*/
 
     @action updateImageData()  {
         if(this.selectedFile != null) {
@@ -127,26 +88,6 @@ export class ImageStore {
             console.log(this.imageData)
         }
     }
-/*
-    @action updateImageDataOld()  {
-        if(this.selectedFile != null) 
-            fs.readFile(this.selectedFile, {
-                    encoding: 'ascii',
-                    flag: 'r'
-                }, action((err: NodeJS.ErrnoException, data:string) => {
-                    let parseResults = Papa.parse(data, {
-                        delimiter: "\t",
-                        header: true,
-                        dynamicTyping: true
-                        //skipEmptyLines: true
-                    })
-                    this.parseData(parseResults)
-                    //this.channelNames = parseResults.meta.fields
-                })
-            )
-    }*/
-
-
   
     @action setChannelDomain = (name: ChannelName) => {
         return action((value: [number, number]) => {
