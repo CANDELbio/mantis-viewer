a5b88c2b0c934ee4a1d746f26a7c81f06bbea172
Robert Schiemann <bobbyrj3d@gmail.com>
2019-07-15 13:21:00 -0600

    Update ConfigurationHelper tests.

--- src/lib/ConfigurationHelper.spec.ts
+++ src/lib/ConfigurationHelper.spec.ts
@@ -1,8 +1,9 @@
-import { getDefaultChannelMarkers, getDefaultChannelDomains } from './ConfigurationHelper'
+import { ConfigurationHelper } from './ConfigurationHelper'
 
 describe('getDefaultChannelDomains', () => {
     it('should the default channel domains', () => {
-        let result = getDefaultChannelDomains()
+        let helper = new ConfigurationHelper()
+        let result = helper.getDefaultChannelDomains()
         expect(result['rChannel']).toEqual([0, 0.7])
         expect(result['gChannel']).toEqual([0, 0.7])
         expect(result['bChannel']).toEqual([0, 0.7])
@@ -11,7 +12,8 @@ describe('getDefaultChannelDomains', () => {
 
 describe('getDefaultChannelMarkers', () => {
     it('should pick default channel markers by priority if they are present', () => {
-        let result = getDefaultChannelMarkers(['FOO', 'CD8', 'CD4', 'CD20', 'BLAH'])
+        let helper = new ConfigurationHelper()
+        let result = helper.getDefaultChannelMarkers(['FOO', 'CD8', 'CD4', 'CD20', 'BLAH'])
         expect(result['rChannel']).toEqual('CD4')
         expect(result['gChannel']).toEqual('CD8')
         expect(result['bChannel']).toEqual('FOO')
