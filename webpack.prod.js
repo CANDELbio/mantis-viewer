2c88fed63d7288b99b80760a75e14924168b5efd
Robert Schiemann <bobbyrj3d@gmail.com>
2021-04-09 15:55:47 -0700

    Fix production webpack better-sqlite3 issue.

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,4 +4,10 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'production',
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: 'source-map',
+    optimization: {
+        // Optimization from production mode that breaks better-sqlite3 in web workers so we disable it.
+        minimize: false,
+    },
 })
