2125b0d2bb024d881fcff82f6c015641934759dc
Robert Schiemann <bobbyrj3d@gmail.com>
2021-08-06 10:45:56 -0700

    Move from bundled geotiff to newer version of geotiff installed from NPM.

--- src/lib/TiffUtils.ts
+++ src/lib/TiffUtils.ts
@@ -1,10 +1,8 @@
-import * as fs from 'fs'
-
 // Importing from a file for now. This package is available on npm, but hasn't been updated in a few months.
 // The newest version on github supports a compression format that a user requested support for.
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 //@ts-ignore
-import GeoTIFF = require('../modules/geotiff.bundle.min.js')
+import GeoTIFF = require('geotiff')
 
 // If any dimension is over maxWidthHeight, we scale it proportionally so the max dimension is equal to scaledWidthHeight.
 // Images in the 10000-12000 width/height range seem to load fine, but GeoTiff returned a black image when attempting to
@@ -38,8 +36,7 @@ function scaleWidthAndHeight(
 }
 
 export async function readTiffData(filepath: string, imageNumber: number): Promise<TiffData> {
-    const rawData = fs.readFileSync(filepath)
-    const tiff = await GeoTIFF.fromArrayBuffer(rawData.buffer)
+    const tiff = await GeoTIFF.fromFile(filepath)
 
     const numImages = await tiff.getImageCount()
     const image = await tiff.getImage(imageNumber)
@@ -48,6 +45,8 @@ export async function readTiffData(filepath: string, imageNumber: number): Promi
     const scaleResults = scaleWidthAndHeight(image.getWidth(), image.getHeight())
 
     const data = await image.readRasters(scaleResults.rasterOptions)
+
+    tiff.close()
 
     return {
         data: data[0],
