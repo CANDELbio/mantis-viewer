eab214aa0a4705ac9b1a2c8ade47586969ce7246
Robert Schiemann <bobbyrj3d@gmail.com>
2023-12-13 12:29:42 -0800

    Refactor generateFillBitmap.

--- src/lib/SegmentationUtils.ts
+++ src/lib/SegmentationUtils.ts
@@ -1,3 +1,46 @@
+import { randomRGBColor } from './ColorHelper'
+
 export function generatePixelMapKey(x: number, y: number): string {
     return x.toString() + '_' + y.toString()
+}
+
+// Generates a texture (to be used to create a PIXI sprite) from segmentation data.
+// Segmentation data is stored as a tiff where the a pixel has a value of 0 if it does not belong to a cell
+// or is a number corresponding to what we're calling the segmentId (i.e. all pixels that belong to cell/segment 1 have a value of 1)
+// Generates a texture (to be used to create a PIXI sprite) from segmentation data.
+// Segmentation data is stored as a tiff where the a pixel has a value of 0 if it does not belong to a cell
+// or is a number corresponding to what we're calling the segmentId (i.e. all pixels that belong to cell/segment 1 have a value of 1)
+export async function generateFillBitmap(
+    segmentIndexMap: Record<number, number[]>,
+    width: number,
+    height: number,
+): Promise<ImageBitmap> {
+    const offScreen = new OffscreenCanvas(width, height)
+
+    const ctx = offScreen.getContext('2d')
+    if (ctx) {
+        // ctx.clearRect(0, 0, offScreen.width, offScreen.height)
+        const imageData = ctx.createImageData(offScreen.width, offScreen.height)
+        const canvasData = imageData.data
+
+        // Here we're iterating through the segmentation data and setting all canvas pixels that have no cell as transparent
+        // or setting all of the pixels belonging to a cell to the same random color with 50% alpha (transparency)
+        for (const segmentId in segmentIndexMap) {
+            const color = randomRGBColor()
+
+            for (const pixel of segmentIndexMap[segmentId]) {
+                // Get the index on the canvas by multiplying by 4 (i.e. bitshifting by 2)
+                const canvasIndex = pixel << 2
+                canvasData[canvasIndex] = color['r']
+                canvasData[canvasIndex + 1] = color['g']
+                canvasData[canvasIndex + 2] = color['b']
+                canvasData[canvasIndex + 3] = 255
+            }
+        }
+        ctx.putImageData(imageData, 0, 0)
+    }
+
+    const bitmap = await createImageBitmap(offScreen)
+
+    return bitmap
 }
--- src/workers/SegmentationDataWorker.worker.ts
+++ src/workers/SegmentationDataWorker.worker.ts
@@ -13,9 +13,8 @@ import {
     SegmentationDataWorkerError,
 } from './SegmentationDataWorker'
 import { OptimizedSegmentationSubfolder } from '../definitions/FileDefinitions'
-import { RGBColorCollection, Coordinate } from '../interfaces/ImageInterfaces'
-import { randomRGBColor } from '../lib/ColorHelper'
-import { generatePixelMapKey } from '../lib/SegmentationUtils'
+import { Coordinate } from '../interfaces/ImageInterfaces'
+import { generatePixelMapKey, generateFillBitmap } from '../lib/SegmentationUtils'
 import { readTiffData } from '../lib/TiffUtils'
 
 function optimizedSegmentationDirectoryPath(segmentationPath: string): string {
@@ -58,73 +57,6 @@ function saveOptimizedSegmentation(
     }
     const dataJSON = JSON.stringify(data)
     fs.writeFileSync(optimizedPath, dataJSON, { encoding: 'utf8' })
-}
-
-function getPixelColor(segmentId: number, colors: RGBColorCollection): { r: number; g: number; b: number } {
-    if (!(segmentId in colors)) {
-        const color = randomRGBColor()
-
-        // Store that color in the colors hash and then return in.
-        colors[segmentId] = color
-        return color
-    }
-
-    //Return the stored color for that segment if it has already been generated.
-    return colors[segmentId]
-}
-
-function drawPixel(segmentId: number, colors: RGBColorCollection, pixel: number, canvasData: Uint8ClampedArray): void {
-    // Tiff data is an array with one index per pixel whereas canvasas have four indexes per pixel (r, g, b, a)
-    // Get the index on the canvas by multiplying by 4 (i.e. bitshifting by 2)
-    const canvasIndex = pixel << 2
-    if (segmentId === 0) {
-        canvasData[canvasIndex] = 0
-        canvasData[canvasIndex + 1] = 0
-        canvasData[canvasIndex + 2] = 0
-        canvasData[canvasIndex + 3] = 0
-    } else {
-        const color = getPixelColor(segmentId, colors)
-        canvasData[canvasIndex] = color['r']
-        canvasData[canvasIndex + 1] = color['g']
-        canvasData[canvasIndex + 2] = color['b']
-        canvasData[canvasIndex + 3] = 255
-    }
-}
-
-// Generates a texture (to be used to create a PIXI sprite) from segmentation data.
-// Segmentation data is stored as a tiff where the a pixel has a value of 0 if it does not belong to a cell
-// or is a number corresponding to what we're calling the segmentId (i.e. all pixels that belong to cell/segment 1 have a value of 1)
-// Generates a texture (to be used to create a PIXI sprite) from segmentation data.
-// Segmentation data is stored as a tiff where the a pixel has a value of 0 if it does not belong to a cell
-// or is a number corresponding to what we're calling the segmentId (i.e. all pixels that belong to cell/segment 1 have a value of 1)
-async function generateFillBitmap(
-    v: Float32Array | Uint16Array | Uint8Array | number[],
-    width: number,
-    height: number,
-): Promise<ImageBitmap> {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-ignore
-    const offScreen = new OffscreenCanvas(width, height)
-
-    // Hash to store the segmentID to randomly generated color mapping
-    const colors = {}
-
-    const ctx = offScreen.getContext('2d')
-    if (ctx) {
-        const imageData = ctx.getImageData(0, 0, offScreen.width, offScreen.height)
-        const canvasData = imageData.data
-
-        // Here we're iterating through the segmentation data and setting all canvas pixels that have no cell as transparent
-        // or setting all of the pixels belonging to a cell to the same random color with 50% alpha (transparency)
-        for (let i = 0; i < v.length; ++i) {
-            drawPixel(v[i], colors, i, canvasData)
-        }
-        ctx.putImageData(imageData, 0, 0)
-    }
-
-    const bitmap = await createImageBitmap(offScreen)
-
-    return bitmap
 }
 
 function generateOutlineMap(segmentLocationMap: Record<number, Coordinate[]>): Record<number, Coordinate[]> {
@@ -228,7 +160,7 @@ async function loadTiffData(
         const centroidMap = calculateCentroids(maps.segmentLocationMap)
         // Generate an ImageBitmap from the tiffData
         // ImageBitmaps are rendered canvases can be passed between processes
-        const bitmap = await generateFillBitmap(data, width, height)
+        const bitmap = await generateFillBitmap(segmentIndexMap, width, height)
 
         if (optimize) {
             saveOptimizedSegmentation(
@@ -359,7 +291,7 @@ async function loadTextData(
 
         // Generate an ImageBitmap from the tiffData
         // ImageBitmaps are rendered canvases can be passed between processes
-        const bitmap = await generateFillBitmap(pixelData, width, height)
+        const bitmap = await generateFillBitmap(segmentIndexMap, width, height)
 
         if (optimize) {
             saveOptimizedSegmentation(
@@ -434,7 +366,7 @@ async function loadOptimizedSegmentation(
 
         // Generate an ImageBitmap from the tiffData
         // ImageBitmaps are rendered canvases can be passed between processes
-        const bitmap = await generateFillBitmap(pixelData, width, height)
+        const bitmap = await generateFillBitmap(segmentIndexMap, width, height)
 
         return {
             filepath: filepath,
